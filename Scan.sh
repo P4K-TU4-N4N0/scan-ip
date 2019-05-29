ab1488538abd3ddd2dd58aff2d0713bd97477952
P4K-TU4-N4N0 <51157995+P4K-TU4-N4N0@users.noreply.github.com>
2019-05-29 20:08:10 +0700

    Create Scan.sh

---
+++ Scan.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#simple kok
+#mau recode
+
+
+
+clear
+
+
+echo "=====================================" | lolcat
+echo "        author by P4K TU4 N4N0       " | lolcat
+echo "          C L 4 Y HACKER T34M        " | lolcat
+echo "  KALAU ADA PERTANYAAN DM DI IG AJA  " | lolcat
+echo "=====================================" | lolcat
+
+
+echo "[!] JANGAN DISALAH GUNAKAN CUK" | lolcat
+echo "[!] gunkan vpn cuk biar aman sedikit" | lolcat
+echo "[√] masukan website target (www.target.com) : " $target | lolcat
+
+
+echo "╭─P4K-TU4-N4N0[masukin websitenya" | lolcat
+read target
+ping $ target
